fbc8269e41ecddbca9612ff8ea45308d0901bdae
Sophia <sherman205@gmail.com>
2020-01-23 19:37:07 -0800

    Force rename git MapView in git

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import MapView from './views/mapview/MapView';
+import MapView from './views/MapView/MapView';
 import './App.scss';
 
 function App() {
