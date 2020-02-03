bd06a2e704d1510ca7fd43fa67cf815f5b7e4d46
Ryan <rwolfe22@gmail.com>
2020-02-02 16:59:59 -0800

    Added save location button

--- src/views/LocationDetails/LocationDetails.js
+++ src/views/LocationDetails/LocationDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Divider } from 'semantic-ui-react';
+import { Divider, Button, Icon, Label } from 'semantic-ui-react';
+import { ReactComponent as SavedPin } from '../../assets/svg/saved_pin.svg';
 
 import './LocationDetails.scss';
 
@@ -7,10 +8,14 @@ import './LocationDetails.scss';
 class LocationDetails extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            saved: false,
+        }
     }
 
     render() {
         const { searchResults } = this.props;
+        const { saved } = this.state;
 
         if (searchResults.text) {
 
@@ -34,6 +39,14 @@ class LocationDetails extends Component {
                     <h4 className="location-details-subheader my-sm">{state} | {country}</h4>
                     <Divider />
                     <p>Bunch of content related to location goes here</p>
+                    <Button as='div' labelPosition='right' size='mini' onClick={() => this.setState({ saved: true })}>
+                        <Button basic color={`${saved ? 'blue' : 'red'}`} size='mini' className="flex flex-column justify-center">
+                            <SavedPin />
+                        </Button>
+                        <Label as='a' color={`${saved ? 'blue' : 'red'}`} size='mini' pointing='left'>
+                            {saved ? 'Location pinned' : 'Pin this location'}
+                        </Label>
+                    </Button>
                 </div>
             )
         }
