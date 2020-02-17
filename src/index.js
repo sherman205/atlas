a3796c2d4133381475555a7b489171890e053f4e
Ryan <rwolfe22@gmail.com>
2020-02-17 14:52:04 -0800

    Fixed custom font import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./js/store/index";
 import App from "./App";
+import './index.scss';
 
 render(
     <Provider store={store}>
